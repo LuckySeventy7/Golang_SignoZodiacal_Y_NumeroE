60851d0b2bf80b766839574bb0a0ab250738ef63
Sergio Ruiz <sergio.rruvalcaba@alumnos.udg.mx>
2020-09-30 20:35:45 -0500

    calculate zodiac sign

--- zodiacal.go
+++ zodiacal.go
@@ -7,71 +7,69 @@ func main() {
 	var mes int
 
 	//fmt.Println("Ingrese, ")
-	fmt.Println("Ingrese dia de nacimiento: ")
-	fmt.Scan(&dia)
-	
 
-	fmt.Println("Ingrese mes de nacimiento: ")
+	fmt.Scan(&dia)
+
 	fmt.Scan(&mes)
 	switch mes{
 		case 1:{
 			if dia >=21 && dia <=31{
-				fmt.Println("Acuario")
+				fmt.Println("acuario")
 			}else if dia <=20 && dia > 0{
-				fmt.Println("Capricomio")
+				fmt.Println("capricomio")
 				}
 			
 		}
 		case 2: {
 			if dia >=20 && dia <=29{
-				fmt.Println("Piscis")
+				fmt.Println("piscis")
 			}else if dia <=19 && dia > 0{
-				fmt.Println("Acuario")
+				fmt.Println("acuario")
 			}
 		}
 		case 3: {
 			if dia >=21 && dia <=31{
-				fmt.Println("Aries")
+				fmt.Println("aries")
 			}else if dia <=20 && dia > 0{
-				fmt.Println("Piscis")
+				fmt.Println("piscis")
 			}
 			
 		}
 		case 4:  {
 			if dia >=21 && dia <=30{
-				fmt.Println("Tauro")
+				fmt.Println("tauro")
 			}else if dia <=20 && dia > 0{
-				fmt.Println("Aries")
+				fmt.Println("aries")
 			}
 		
 		}
 		case 5: {
 			if dia >=21 && dia <=31{
-				fmt.Println("Geminis")
+				fmt.Println("geminis")
 			}else if dia <=20 && dia > 0{
-				fmt.Println("Tauro")
+				fmt.Println("tauro")
 			}
 		
 		}
 		case 6: {
 			if dia >=22 && dia <=30{
-				fmt.Println("Cancer")
+				fmt.Println("cancer")
 			}else if dia <=21 && dia > 0{
-				fmt.Println("Gemisis")
+				fmt.Println("geminis")
 			}
 			
 		}
 		case 7: {
 			if dia >=23 && dia <=31{
-				fmt.Println("Leo")
+				fmt.Println("leo")
 			}else if dia <=22 && dia > 0{
-				fmt.Println("Cancer")
+				fmt.Println("cancer")
 			}
 			
 		}
 		case 8: {
 			if dia >=24 && dia <=30{
-				fmt.Println("Virgo")
+				fmt.Println("virgo")
 			}else if dia <=23 && dia > 0{
 				fmt.Println("Leo")
 			}
@@ -79,33 +77,33 @@ func main() {
 		}
 		case 9:{
 			if dia >=23 && dia <=30{
-				fmt.Println("Libra")
+				fmt.Println("libra")
 			}else if dia <=22 && dia > 0{
-				fmt.Println("Virgo")
+				fmt.Println("virgo")
 			}
 			
 		}
 		case 10: {
 			if dia >=23 && dia <=31{
-				fmt.Println("Escorpio")
+				fmt.Println("escorpio")
 			}else if dia <=22 && dia > 0{
-				fmt.Println("Libra")
+				fmt.Println("libra")
 			}
 			
 		}
 		case 11: {
 			if dia >=23 && dia <=30{
-				fmt.Println("Sagitario")
+				fmt.Println("sagitario")
 			}else if dia <=22 && dia > 0{
-				fmt.Println("Escorpio")
+				fmt.Println("escorpio")
 			}
 		
 		}
 		case 12: {
 			if dia >=22 && dia <=31{
-				fmt.Println("Capriomio")
+				fmt.Println("capriomio")
 			}else if dia <=21 && dia > 0{
-				fmt.Println("Sagitario")
+				fmt.Println("sagitario")
 			}
 		
 		}
